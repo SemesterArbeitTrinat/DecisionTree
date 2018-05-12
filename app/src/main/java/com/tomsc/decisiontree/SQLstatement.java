108a49a995f09be6ce476da77ff040999e4619e7
Thomas Schaad <tomy.schaad@gmail.com>
2018-05-12 12:11:29 +0200

    example SQL statement added -> causing errors

--- app/src/main/java/com/tomsc/decisiontree/SQLstatement.java
+++ app/src/main/java/com/tomsc/decisiontree/SQLstatement.java
@@ -1,5 +1,9 @@
 package com.tomsc.decisiontree;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
 /**
  * Created by tomsc on 10.05.2018.
  * Class to do all the SQLstatement with unified methods
@@ -29,4 +33,38 @@ public class SQLstatement {
     {
 
     }
+
+    SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+    // Define a projection that specifies which columns from the database
+// you will actually use after this query.
+    String[] projection = {
+            BaseColumns._ID,
+            FeedEntry.COLUMN_NAME_TITLE,
+            FeedEntry.COLUMN_NAME_SUBTITLE
+    };
+
+    // Filter results WHERE "title" = 'My Title'
+    String selection = FeedEntry.COLUMN_NAME_TITLE + " = ?";
+    String[] selectionArgs = { "My Title" };
+
+    // How you want the results sorted in the resulting Cursor
+    String sortOrder =
+            FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
+
+    Cursor cursor;
+
+    {
+        cursor = db.query(
+                FeedEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+    }
+
+
 }
